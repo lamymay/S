deb19313c746e0d67c2245dd464b3b5e0c995805
lamymay <lamymay@outlook.com>
2020-02-09 13:08:16 +0800

    跳转后返回数据，即：a跳到b，b处理完成后返回a并带上数据给a

--- app/src/main/java/com/arc/s/MainActivity.java
+++ app/src/main/java/com/arc/s/MainActivity.java
@@ -1,7 +1,8 @@
 package com.arc.s;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -16,17 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        //A to B  ,B is this START
+//        int v1 = getIntent().getIntExtra("key1", 0);//接收int类型的数字，如果为null需要给默认值，基本数据类型注意给出默认值
+//        String v2 = getIntent().getStringExtra("key2");//接收String数据
+//        Toast.makeText(this, "接收到的参数" + v1 + v2, Toast.LENGTH_SHORT).show();//Toast 用于弹出提示
+//        //A to B  ,B is this  END
 
 
-        //A to B  ,B is this
-        int v1 = getIntent().getIntExtra("key1", 0);//接收int类型的数字，如果为null需要给默认值，基本数据类型注意给出默认值
-        String v2 = getIntent().getStringExtra("key2");//接收String数据
-        Toast.makeText(this, "接收到的参数" + v1 + v2, Toast.LENGTH_SHORT).show();//Toast 用于弹出提示
-        //        Intent intent = new Intent( );
-        //        intent.putExtra("int", 123456);
-        //        intent.putExtra("s2", "String 啦啦啦");
-        //        setResult(Activity.RESULT_OK,intent);
-        //结束当前页面
-        //        finish();
+        Intent intent = new Intent();//在这个地方返回数据
+        intent.putExtra("bKey1", 4444);
+        intent.putExtra("bKey2", "返回值是 String 啦啦啦");
+        setResult(Activity.RESULT_OK, intent);//返回成功并带上数据
+        finish(); //结束当前页面
     }
 }
--- app/src/main/java/com/arc/s/WelcomeActivity.java
+++ app/src/main/java/com/arc/s/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 /**
  * @author 叶超
@@ -26,25 +27,33 @@ public class WelcomeActivity extends Activity {
                         //从一个a activity 跳转 b activity 并携带参数
                         intent.putExtra("key1", 123456);
                         intent.putExtra("key2", "String type");
-                        // startActivityForResult(intent, 0x10);  // 收到返回值
+                        startActivityForResult(intent, 0x10);  //此行代码表示跳转后，想要拿到返回值
                         startActivity(intent);
                     }
                 }, 2000);
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        //注意是两个code 别判断错了
-//        if (requestCode == 0x10) {
-//            if (resultCode == Activity.RESULT_OK) {
-//                // B  to A ,A is this
-//                // 注意是 data 中get
-//                int v1 = data.getIntExtra("int", 0);//基本数据类型注意给出默认值}
-//                String v2 = data.getStringExtra("s2");
-//                Toast.makeText(this, "接收到的参数" + v1 + v2, Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//
-//    }
+    /**
+     * 用这个函数来接收返回值
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //注意是两个code 别判断错了
+        if (requestCode == 0x10) { // 这个地方填的是我上面填的值  0x10
+            if (resultCode == Activity.RESULT_OK) {
+                // B  to A ,A is this
+                // 注意是 data 中get
+                int val1FromB = data.getIntExtra("bKey1", 0); //基本数据类型注意给出默认值}
+                String val2FromB = data.getStringExtra("bKey2");
+                Toast.makeText(this, "接收到的参数" + val1FromB + val2FromB, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
+
+
